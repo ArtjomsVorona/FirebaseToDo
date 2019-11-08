7db239fd13218cf7afbe9415b5ab0a32396ed346
Artjoms Vorona <artjomsvorona@gmail.com>
2019-11-08 13:24:05 +0200

    Initial Commit

---
+++ FirebaseToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseToDo
+//
+//  Created by Artjoms Vorona on 08/11/2019.
+//  Copyright © 2019 Artjoms Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
